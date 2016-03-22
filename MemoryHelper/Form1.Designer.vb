13dd12f5c8f4abb165c581ffa3bb2a09febc7edf
Adam Umiecki <aumiecki@gmail.com>
2016-03-22 18:39:38 -0500

    Week 4 iLab

---
+++ MemoryHelper/Form1.Designer.vb
@@ -0,0 +1,318 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tbxTitle = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dtpDate = New System.Windows.Forms.DateTimePicker()
+        Me.tbxMemo = New System.Windows.Forms.TextBox()
+        Me.cbxDeleted = New System.Windows.Forms.CheckBox()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveAsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.tsbFirst = New System.Windows.Forms.ToolStripButton()
+        Me.tsbPrev = New System.Windows.Forms.ToolStripButton()
+        Me.tsbNext = New System.Windows.Forms.ToolStripButton()
+        Me.tsbLast = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tsbSearch = New System.Windows.Forms.ToolStripButton()
+        Me.tsbEdit = New System.Windows.Forms.ToolStripButton()
+        Me.tsbAdd = New System.Windows.Forms.ToolStripButton()
+        Me.tsbDelete = New System.Windows.Forms.ToolStripButton()
+        Me.testTmp = New System.Windows.Forms.Button()
+        Me.MenuStrip.SuspendLayout()
+        Me.StatusStrip.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 105)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Title:"
+        '
+        'tbxTitle
+        '
+        Me.tbxTitle.Location = New System.Drawing.Point(69, 97)
+        Me.tbxTitle.Name = "tbxTitle"
+        Me.tbxTitle.Size = New System.Drawing.Size(208, 20)
+        Me.tbxTitle.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 139)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(33, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Date:"
+        '
+        'dtpDate
+        '
+        Me.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpDate.Location = New System.Drawing.Point(69, 139)
+        Me.dtpDate.Name = "dtpDate"
+        Me.dtpDate.Size = New System.Drawing.Size(208, 20)
+        Me.dtpDate.TabIndex = 5
+        '
+        'tbxMemo
+        '
+        Me.tbxMemo.Location = New System.Drawing.Point(19, 220)
+        Me.tbxMemo.Multiline = True
+        Me.tbxMemo.Name = "tbxMemo"
+        Me.tbxMemo.Size = New System.Drawing.Size(495, 148)
+        Me.tbxMemo.TabIndex = 6
+        '
+        'cbxDeleted
+        '
+        Me.cbxDeleted.AutoSize = True
+        Me.cbxDeleted.Location = New System.Drawing.Point(451, 100)
+        Me.cbxDeleted.Name = "cbxDeleted"
+        Me.cbxDeleted.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.cbxDeleted.Size = New System.Drawing.Size(63, 17)
+        Me.cbxDeleted.TabIndex = 9
+        Me.cbxDeleted.Text = "Deleted"
+        Me.cbxDeleted.UseVisualStyleBackColor = True
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem, Me.NewToolStripMenuItem, Me.SaveToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(37, 20)
+        Me.ToolStripMenuItem1.Text = "File"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SaveAsToolStripMenuItem})
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'SaveAsToolStripMenuItem
+        '
+        Me.SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        Me.SaveAsToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.SaveAsToolStripMenuItem.Text = "Save As"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'MenuStrip
+        '
+        Me.MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1})
+        Me.MenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip.Name = "MenuStrip"
+        Me.MenuStrip.Size = New System.Drawing.Size(591, 24)
+        Me.MenuStrip.TabIndex = 10
+        '
+        'StatusStrip
+        '
+        Me.StatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 396)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.Size = New System.Drawing.Size(591, 22)
+        Me.StatusStrip.TabIndex = 11
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(101, 17)
+        Me.ToolStripStatusLabel1.Text = "Current Record = "
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsbFirst, Me.tsbPrev, Me.tsbNext, Me.tsbLast, Me.ToolStripSeparator1, Me.tsbSearch, Me.tsbEdit, Me.tsbAdd, Me.tsbDelete})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 24)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(591, 38)
+        Me.ToolStrip1.TabIndex = 12
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'tsbFirst
+        '
+        Me.tsbFirst.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbFirst.Image = CType(resources.GetObject("tsbFirst.Image"), System.Drawing.Image)
+        Me.tsbFirst.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbFirst.Name = "tsbFirst"
+        Me.tsbFirst.Size = New System.Drawing.Size(23, 35)
+        Me.tsbFirst.Text = "First Record"
+        '
+        'tsbPrev
+        '
+        Me.tsbPrev.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbPrev.Image = CType(resources.GetObject("tsbPrev.Image"), System.Drawing.Image)
+        Me.tsbPrev.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbPrev.Name = "tsbPrev"
+        Me.tsbPrev.Size = New System.Drawing.Size(23, 35)
+        Me.tsbPrev.Text = "Previous Record"
+        '
+        'tsbNext
+        '
+        Me.tsbNext.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbNext.Image = CType(resources.GetObject("tsbNext.Image"), System.Drawing.Image)
+        Me.tsbNext.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbNext.Name = "tsbNext"
+        Me.tsbNext.Size = New System.Drawing.Size(23, 35)
+        Me.tsbNext.Text = "Next Record"
+        '
+        'tsbLast
+        '
+        Me.tsbLast.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbLast.Image = CType(resources.GetObject("tsbLast.Image"), System.Drawing.Image)
+        Me.tsbLast.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbLast.Name = "tsbLast"
+        Me.tsbLast.Size = New System.Drawing.Size(23, 35)
+        Me.tsbLast.Text = "Last Record"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 38)
+        '
+        'tsbSearch
+        '
+        Me.tsbSearch.Image = CType(resources.GetObject("tsbSearch.Image"), System.Drawing.Image)
+        Me.tsbSearch.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbSearch.Name = "tsbSearch"
+        Me.tsbSearch.Size = New System.Drawing.Size(45, 35)
+        Me.tsbSearch.Text = "search"
+        Me.tsbSearch.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'tsbEdit
+        '
+        Me.tsbEdit.Image = CType(resources.GetObject("tsbEdit.Image"), System.Drawing.Image)
+        Me.tsbEdit.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbEdit.Name = "tsbEdit"
+        Me.tsbEdit.Size = New System.Drawing.Size(31, 35)
+        Me.tsbEdit.Text = "edit"
+        Me.tsbEdit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'tsbAdd
+        '
+        Me.tsbAdd.Image = CType(resources.GetObject("tsbAdd.Image"), System.Drawing.Image)
+        Me.tsbAdd.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbAdd.Name = "tsbAdd"
+        Me.tsbAdd.Size = New System.Drawing.Size(31, 35)
+        Me.tsbAdd.Text = "add"
+        Me.tsbAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'tsbDelete
+        '
+        Me.tsbDelete.Image = CType(resources.GetObject("tsbDelete.Image"), System.Drawing.Image)
+        Me.tsbDelete.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbDelete.Name = "tsbDelete"
+        Me.tsbDelete.Size = New System.Drawing.Size(43, 35)
+        Me.tsbDelete.Text = "delete"
+        Me.tsbDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'testTmp
+        '
+        Me.testTmp.Location = New System.Drawing.Point(439, 134)
+        Me.testTmp.Name = "testTmp"
+        Me.testTmp.Size = New System.Drawing.Size(75, 23)
+        Me.testTmp.TabIndex = 13
+        Me.testTmp.Text = "Test"
+        Me.testTmp.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(591, 418)
+        Me.Controls.Add(Me.testTmp)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.StatusStrip)
+        Me.Controls.Add(Me.cbxDeleted)
+        Me.Controls.Add(Me.tbxMemo)
+        Me.Controls.Add(Me.dtpDate)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.tbxTitle)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "Form1"
+        Me.Text = "Memory Helper"
+        Me.MenuStrip.ResumeLayout(False)
+        Me.MenuStrip.PerformLayout()
+        Me.StatusStrip.ResumeLayout(False)
+        Me.StatusStrip.PerformLayout()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tbxTitle As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents dtpDate As DateTimePicker
+    Friend WithEvents tbxMemo As TextBox
+    Friend WithEvents cbxDeleted As CheckBox
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MenuStrip As MenuStrip
+    Friend WithEvents StatusStrip As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents tsbFirst As ToolStripButton
+    Friend WithEvents tsbPrev As ToolStripButton
+    Friend WithEvents tsbNext As ToolStripButton
+    Friend WithEvents tsbLast As ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents tsbSearch As ToolStripButton
+    Friend WithEvents tsbEdit As ToolStripButton
+    Friend WithEvents tsbAdd As ToolStripButton
+    Friend WithEvents tsbDelete As ToolStripButton
+    Friend WithEvents testTmp As Button
+End Class
---
+++ MemoryHelper/Form1.vb
@@ -0,0 +1,146 @@
+﻿Public Class Form1
+
+    Dim strFilePathName As String = ""
+    Dim intCurrentRecord As Integer
+
+    Structure MemoryRecord
+        Dim blnDeleted As Boolean
+        Dim dDate As Date
+        Dim strTitle As String
+        Dim strMemo As String
+    End Structure
+
+    Dim dataTable(-1) As MemoryRecord
+
+    'Private Sub testTmp_Click(sender As Object, e As EventArgs) Handles testTmp.Click
+    '    'Temp Event handler used to test
+    '    clearDisplay()
+    'End Sub
+
+    '*** Helper Procedures ***
+
+    Private Sub allowEditRecord()
+        Me.tbxTitle.Enabled = True
+        Me.tbxMemo.Enabled = True
+        Me.dtpDate.Enabled = True
+        Me.cbxDeleted.Enabled = False
+    End Sub
+
+    Private Sub protectRecord()
+        Me.tbxTitle.Enabled = False
+        Me.tbxMemo.Enabled = False
+        Me.dtpDate.Enabled = False
+        Me.cbxDeleted.Enabled = False
+    End Sub
+
+    Private Sub clearDisplay()
+        Me.tbxTitle.Text = String.Empty
+        Me.tbxMemo.Text = String.Empty
+        Me.dtpDate.Value = Today.Date
+        Me.cbxDeleted.Checked = False
+    End Sub
+
+    Private Sub updatePosition()
+        Me.StatusStrip.Text = "Current Record = " & (intCurrentRecord + 1).ToString & " of " & dataTable.Length.ToString
+    End Sub
+    '*** Worker Procedures ***
+
+    Private Sub newRecord()
+        saveRecord()
+        clearDisplay()
+        ReDim Preserve dataTable(dataTable.Length)
+        intCurrentRecord = dataTable.GetUpperBound(0)
+        allowEditRecord()
+        updatePosition()
+    End Sub
+
+    Private Sub deleteRecord()
+        dataTable(intCurrentRecord).blnDeleted = True
+        cbxDeleted.Checked = True
+        protectRecord()
+    End Sub
+
+    Private Sub saveRecord()
+        If intCurrentRecord >= 0 Then
+            dataTable(intCurrentRecord).strTitle = Me.tbxTitle.Text
+            dataTable(intCurrentRecord).strMemo = Me.tbxMemo.Text
+            dataTable(intCurrentRecord).dDate = Me.dtpDate.Value
+            dataTable(intCurrentRecord).blnDeleted = Me.cbxDeleted.Checked
+            protectRecord()
+        End If
+    End Sub
+
+    Private Sub emptyRecords()
+        clearDisplay()
+        protectRecord()
+        intCurrentRecord = -1
+        ReDim dataTable(-1)
+    End Sub
+    '*** ToolStrip Event Handlers ***
+
+    Private Sub tsbAdd_Click(sender As Object, e As EventArgs) Handles tsbAdd.Click
+        newRecord()
+    End Sub
+
+    Private Sub tsbEdit_Click(sender As Object, e As EventArgs) Handles tsbEdit.Click
+        allowEditRecord()
+    End Sub
+
+    Private Sub tsbDelete_Click(sender As Object, e As EventArgs) Handles tsbDelete.Click
+        deleteRecord()
+    End Sub
+    '*** Navigation Buttons ***
+
+    Private Sub showRecord(ByVal index As Integer)
+        saveRecord()
+        If index >= 0 And index <= dataTable.GetUpperBound(0) Then
+            Me.tbxTitle.Text = dataTable(index).strTitle
+            Me.tbxMemo.Text = dataTable(index).strMemo
+            Me.dtpDate.Value = dataTable(index).dDate
+            Me.cbxDeleted.Checked = dataTable(index).blnDeleted
+            intCurrentRecord = index
+        End If
+    End Sub
+
+    Private Sub tsbFirst_Click(sender As Object, e As EventArgs) Handles tsbFirst.Click
+        showRecord(0)
+    End Sub
+
+    Private Sub tsbLast_Click(sender As Object, e As EventArgs) Handles tsbLast.Click
+        showRecord(dataTable.Length - 1)
+    End Sub
+
+    Private Sub tsbNext_Click(sender As Object, e As EventArgs) Handles tsbNext.Click
+        showRecord(intCurrentRecord + 1)
+    End Sub
+
+    Private Sub tsbPrev_Click(sender As Object, e As EventArgs) Handles tsbPrev.Click
+        showRecord(intCurrentRecord - 1)
+    End Sub
+
+    Private Function FindRecord(ByVal strSearchTerm As String) As Integer
+        Dim returnValue As Integer = -1
+        Dim currentElement As Integer = 0
+
+        For Each item As MemoryRecord In dataTable
+            If item.strMemo.Contains(strSearchTerm) Or item.strTitle.Contains(strSearchTerm) Then
+                returnValue = currentElement
+                Exit For
+            End If
+            currentElement += 1
+        Next item
+
+        Return returnValue
+    End Function
+
+    Private Sub tsbSearch_Click(sender As Object, e As EventArgs) Handles tsbSearch.Click
+        Dim strSearchTerm As String = InputBox("Search Term:", "Find Record")
+        Dim index As Integer = FindRecord(strSearchTerm)
+
+        If index >= 0 Then
+            showRecord(index)
+        Else
+            MsgBox("Record Not Found", MsgBoxStyle.Critical)
+        End If
+    End Sub
+End Class
